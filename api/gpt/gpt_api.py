c0d7ea50f73ff2dc6d6b5f2d5cec0a113a53408b
Yunel <rahimovyune1@gmail.com>
2024-05-24 15:39:30 +0300

    empty commit

--- api/gpt/gpt_api.py
+++ api/gpt/gpt_api.py
@@ -11,39 +11,40 @@ load_dotenv()
 
 
 client = AsyncOpenAI(
-        api_key=os.getenv('OPENAI_API_KEY'),
-        base_url="https://api.proxyapi.ru/openai/v1"
-    )
+    api_key=os.getenv("OPENAI_API_KEY"), base_url="https://api.proxyapi.ru/openai/v1"
+)
 
 
-async def generate_answer(tg_id:int, fb_id:str, fb_text:str, api_key:str, auto:bool, db=path) -> bool | str:
+async def generate_answer(
+    tg_id: int, fb_id: str, fb_text: str, api_key: str, auto: bool, db=path
+) -> bool | str:
     ruls = 'Напиши благодарственный ответ на отзыв к покупке на маркетплейсе от имени продавца.Ответ должен соответствовать следующим критериям - ответ не должен содержать заполнители, параметры для вставки (наприер {имя клиента|компания} и т.п.); - клиент имеет женский пол; - не должен содержать информацию от кого этот ответ(например: "Команда продавцов" - такого не должно быть в ответе); - не должно быть предложение клиенту об обмене товара или какой-то компенсации. Пример ответа: Здравствуйте. Спасибо за то, что выбрали нас. Позвольте поблагодарить Вас за отзыв. Мы очень рады, что Вам подошел Наш товар, носите с удовольствием. Будем рады видеть Вас снова. Приятных покупок!  Дальше я передаю сам ответ покупателя:   '
-    
+
     try:
         chat_completion = await client.chat.completions.create(
-                messages=[
-                    {
-                        "role": "system",
-                        "content": ruls + fb_text,
-                    }
-                ],
-                model="gpt-3.5-turbo",
-            )
-        
+            messages=[
+                {
+                    "role": "system",
+                    "content": ruls + fb_text,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+
         answer = chat_completion.choices[0].message.content
-        
+
         if auto:
             print("OpenAI ChatGPT сгенерировал ответ и отправил на публикацию")
             if not await answer_feedback(fb_id, answer, api_key):
                 return False
-            
+
             await update_answer_text(fb_id, answer, db)
-            
+
             return True
-        
+
         await update_user_props_after_generating(tg_id, db)
         return answer
-                    
-    except Exception as e: 
+
+    except Exception as e:
         print("Ошибка при генерации ответа на отзыв.")
         return False
--- api/scheduler/scheduler.py
+++ api/scheduler/scheduler.py
@@ -1,9 +1,11 @@
-from openai import AsyncOpenAI
 from api.gpt.gpt_api import generate_answer
 from app.database.shop_settings import get_rating
 from app.database.user_methods import get_users_with_balance
 from app.database.shop_methods import get_apis_list
-from app.database.answer_methods import delete_old_shown_feedback, fill_unanswered_feedback
+from app.database.answer_methods import (
+    delete_old_shown_feedback,
+    fill_unanswered_feedback,
+)
 from api.wb.wb_feedbacks_ans import get_feedbacks
 
 
@@ -12,73 +14,64 @@ async def test_sched() -> None:
 
 
 async def scheduled_db_fill_job() -> bool:
-    
+
     await delete_old_shown_feedback()
-    
+
     users_with_balance = await get_users_with_balance()
-    
+
     if users_with_balance is None:
         print("все пользователи без баланса")
         return None
-    
-    got_apis_for_user_id = {user_id: await get_apis_list(user_id) for user_id in users_with_balance}
-           
+
+    got_apis_for_user_id = {
+        user_id: await get_apis_list(user_id) for user_id in users_with_balance
+    }
+
     for user_id in users_with_balance:
-        
+
         if got_apis_for_user_id[user_id] is None:
             print("по какой-то причине у пользователя нет магазинов")
             continue
-        
-        
+
         # Working with shop's api_key
         for api_key in got_apis_for_user_id[user_id]:
-            
+
             got_rating_from_api = await get_rating(user_id, api_key=api_key)
-            
+
             if got_rating_from_api == None:
-                got_rating_from_api = '0'
+                got_rating_from_api = "0"
                 print("Не удалось вычислить рейтинг магазина: \n", api_key)
-            
+
             got_feedback = await get_feedbacks(api_key)
-            
             if got_feedback is None:
                 continue
-            
+
             # Working with loaded feedbacks from one API
-            for i in range(got_feedback['data']['countUnanswered']):
-                
-                fb_id = got_feedback['data']['feedbacks'][i]['id']
-                fb_rating = got_feedback['data']['feedbacks'][i]['productValuation']
-                fb_shop_wb = got_feedback['data']['feedbacks'][i]['productDetails']['brandName']
-                fb_product_wb = got_feedback['data']['feedbacks'][i]['productDetails']['productName']
-                fb_text = got_feedback['data']['feedbacks'][i]['text']
-            
+            for i in range(got_feedback["data"]["countUnanswered"]):
+
+                fb_id = got_feedback["data"]["feedbacks"][i]["id"]
+                fb_rating = got_feedback["data"]["feedbacks"][i]["productValuation"]
+                fb_shop_wb = got_feedback["data"]["feedbacks"][i]["productDetails"][
+                    "brandName"
+                ]
+                fb_product_wb = got_feedback["data"]["feedbacks"][i]["productDetails"][
+                    "productName"
+                ]
+                fb_text = got_feedback["data"]["feedbacks"][i]["text"]
+
                 if not await fill_unanswered_feedback(
-                    fb_id, 
-                    fb_rating, 
-                    fb_shop_wb, 
-                    fb_product_wb, 
-                    fb_text, 
-                    api_key
+                    fb_id, fb_rating, fb_shop_wb, fb_product_wb, fb_text, api_key
                 ):
                     print("пытаюсь пихнуть существующий в базе отзыв")
                     return False
-                                
-                if got_rating_from_api != '0' or got_rating_from_api == 'gt0':
+
+                if got_rating_from_api != "0" or got_rating_from_api == "gt0":
                     await generate_answer(user_id, fb_id, fb_text, api_key, True)
-                elif got_rating_from_api == 'gt2' and fb_rating >= 2:
+                elif got_rating_from_api == "gt2" and fb_rating >= 2:
                     await generate_answer(user_id, fb_id, fb_text, api_key, True)
-                elif got_rating_from_api == 'gt3' and fb_rating >= 3:
+                elif got_rating_from_api == "gt3" and fb_rating >= 3:
                     await generate_answer(user_id, fb_id, fb_text, api_key, True)
-                elif got_rating_from_api == 'gt4' and fb_rating >= 4:
+                elif got_rating_from_api == "gt4" and fb_rating >= 4:
                     await generate_answer(user_id, fb_id, fb_text, api_key, True)
 
     return True
-            
-            
-            
-                
-            
-        
-        
-        
