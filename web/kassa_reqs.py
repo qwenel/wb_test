d754c4251f76751017478340bc5a7b9f8f98dc8d
Yunel <rahimovyune1@gmail.com>
2024-06-08 22:52:29 +0300

    launching balance_handler funcs from kassa_reqs after payment

--- web/kassa_reqs.py
+++ web/kassa_reqs.py
@@ -1,16 +1,13 @@
-import os
-from dotenv import load_dotenv
 from fastapi import APIRouter, Request
 
-from api.robocassa.robocassa import parse_response, check_signature_result
+from api.robocassa.robocassa import parse_response, check_signature_result, shop_pwd_2
+from app.handlers.balance_handler import payment_status_failure, payment_status_success
 from web.set_webhook import bot
+from app.states.userStates import UserStates
 from main import logger
 
 
 router_kassa = APIRouter()
-
-
-load_dotenv()
 
 
 @router_kassa.get("/kassa/cqtNmI7phruR20R2U8Rz")
@@ -21,7 +18,7 @@ async def result_payment(request: Request) -> str:
     req = request.query_params
     logger.info(f"GOT ROBOKASSA GET REQUEST (ResultURL): {req}")
 
-    merchant_password_2 = os.getenv("SHOP_PWD2_TEST")
+    merchant_password_2 = shop_pwd_2
     param_request = parse_response(str(req))
     cost = param_request["OutSum"]
     number = param_request["InvId"]
@@ -29,6 +26,11 @@ async def result_payment(request: Request) -> str:
 
     if check_signature_result(number, cost, signature, merchant_password_2):
         logger.info("success payment")
+        await payment_status_success(
+            bot, number, cost, UserStates.balance_replenishment
+        )
         return f'OK{param_request["InvId"]}'
+
     logger.info("failure payment")
+    await payment_status_failure(bot, number, cost, UserStates.balance_replenishment)
     return "bad sign"
