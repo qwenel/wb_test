c207da3824aba37e9c69853ac216c4c86a64d5de
Yunel <rahimovyune1@gmail.com>
2024-06-25 02:54:41 +0300

    updating test data set in google sheets correctly

--- api/gspread/gspread.py
+++ api/gspread/gspread.py
@@ -11,4 +11,21 @@ gc = gspread.service_account(filename="cred.json")
 async def perform_gspread_update(data):
     wks = gc.open("feedbacks_bot_data").sheet1
 
-    wks.update(data)
+    wks.update(
+        [
+            [
+                "Дата регистрации",
+                "Username",
+                "Номер телефона",
+                "Количество магазинов",
+                "API-ключи",
+                "Количество ответов",
+                "Дата последнего ответа",
+                "Баланс",
+                "Дата последней покупки",
+                "Сумма всех покупок",
+            ]
+        ],
+        "A1",
+    )
+    wks.update(data, "A2")
